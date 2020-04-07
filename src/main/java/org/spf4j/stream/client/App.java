6a102bfded68acf6c4263f27bd04bb823f89e5cb
Zoltan Farkas <zolyfarkas@yahoo.com>
2020-04-07 06:38:28 -0400

    [add] profiler

--- src/main/java/org/spf4j/stream/client/App.java
+++ src/main/java/org/spf4j/stream/client/App.java
@@ -3,6 +3,7 @@ package org.spf4j.stream.client;
 
 import java.io.IOException;
 import org.openimaj.video.capture.VideoCaptureException;
+import org.spf4j.stackmonitor.Sampler;
 
 /**
  * OpenIMAJ Hello world!
@@ -12,6 +13,8 @@ public class App {
 
   public static void main(String[] args) throws VideoCaptureException, IOException, InterruptedException {
     Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionDisplayer());
+    Sampler sampler = new Sampler(10);
+    sampler.registerJmx();
     java.awt.EventQueue.invokeLater(new Runnable() {
       @Override
       public void run() {
